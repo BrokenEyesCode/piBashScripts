62e8c6838ee2cf482ca51406f7d976230b3ca7db
Leona <github@lhatkins.co.uk>
2017-09-25 21:04:04 +0100

    Delete vMerge.sh

--- vMerge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-videoDir="/home/pi/motioneye/output"
-date=$(date +%Y-%m-%d)
-filesList=""
-for file in $(ls $videoDir/$date/*.avi|sort -n);do
-    filesList="$filesList -cat $file"
-done
-echo $filesList >> $videoDir/$date/files.txt
